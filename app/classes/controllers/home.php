7532d1c028021588ddb5a59a1c2bcec60e85e238
roine <feunetre@hotmail.com>
2012-12-21 16:54:10 +0800

    init

--- app/classes/controllers/home.php
+++ app/classes/controllers/home.php
@@ -3,7 +3,7 @@ class Home extends Base{
 
 	public function view_index($params){
 		echo 'homepage';
-		Model_home::init('prout')->find_all();
+		Model_home::init()->find_all();
 		if(count($params) > 1)
 			echo 'hello '.implode(',',$params);
 	}
--- core/models.php
+++ core/models.php
@@ -3,11 +3,15 @@
 class Orm{
 
 	protected static $tablename = '';
-	private static $_instance;
-	private static $config = [];
 
+
+	public static function find_all(){
+		echo self::$tablename;
+		
+	}
 	
-	private function __construct($tablename){
+	public static function init($tablename = null){
+
 		if(gettype($tablename) == 'string' && !empty($tablename)){
 			// if table defined then set the table name
 			self::$tablename = $tablename;
@@ -16,23 +20,11 @@ class Orm{
 			// ...else get the table name from the model name
 			self::$tablename = implode(',', self::$tablename = array_slice(explode('_', get_called_class()), 1));
 		}
-	}
-
-	public static function find_all(){
-		echo self::$tablename;
-		
-	}
-
-	public static function init($tablename = null){
-
 		// PDO init
 		$ini = ROOT.'/config/database/config.ini' ;
 		$config = parse_ini_file ( $ini , true );
 		
-		// Singleton
-		if (!isset(self::$_instance)){
-            self::$_instance = new self($tablename);
-        }
-        return self::$_instance;
+		
+		return new self;
 	}
 }
